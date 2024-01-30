2c71c3fbdb5c23149692ebc8bccc075dbaa95aa1
Wilson Cusack <wilson.cusack@coinbase.com>
2024-01-30 16:47:21 -0500

    cleanup

--- src/FrameVerifier.sol
+++ src/FrameVerifier.sol
@@ -1,19 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.9;
 
-import {
-    MessageDataCodec,
-    MessageData,
-    MessageType,
-    ReactionType,
-    CastId
-} from "farcaster-solidity/protobufs/message.proto.sol";
+import {Blake3} from "./libraries/Blake3.sol";
+import {Ed25519} from "./libraries/Ed25519.sol";
+import {MessageData, MessageDataCodec} from "./Encoder.sol";
 
-import {Blake3} from "farcaster-solidity/libraries/Blake3.sol";
-
-import {Ed25519} from "farcaster-solidity/libraries/Ed25519.sol";
-
-library FarcasterSolidity {
+library FrameVerifier {
     error InvalidSignature();
     error InvalidEncoding();
     error InvalidMessageType();
@@ -30,12 +22,12 @@ library FarcasterSolidity {
         return Ed25519.verify(public_key, signature_r, signature_s, message_hash);
     }
 
-    function verifyFrameActionBodyMessage(
+    function verifyMessageData(
         bytes32 public_key,
         bytes32 signature_r,
         bytes32 signature_s,
-        bytes memory message
-    ) external {
-        _verifyMessage(public_key, signature_r, signature_s, message);
+        MessageData memory messageData
+    ) external pure {
+        _verifyMessage(public_key, signature_r, signature_s, MessageDataCodec.encode(messageData));
     }
 }
