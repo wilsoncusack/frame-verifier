ff0e83d040676c5323c8173d919e744a86cfd184
Wilson Cusack <wilson.cusack@coinbase.com>
2024-01-30 16:31:25 -0500

    checkin

--- src/FarcasterSolidity.sol
+++ src/FarcasterSolidity.sol
@@ -2,110 +2,40 @@
 pragma solidity ^0.8.9;
 
 import {
-  MessageDataCodec,
-  MessageData,
-  MessageType,
-  ReactionType,
-  CastId
-} from 'farcaster-solidity/protobufs/message.proto.sol';
+    MessageDataCodec,
+    MessageData,
+    MessageType,
+    ReactionType,
+    CastId
+} from "farcaster-solidity/protobufs/message.proto.sol";
 
-import {
-  Blake3
-} from 'farcaster-solidity/libraries/Blake3.sol';
+import {Blake3} from "farcaster-solidity/libraries/Blake3.sol";
 
-import {
-  Ed25519
-} from 'farcaster-solidity/libraries/Ed25519.sol';
-
+import {Ed25519} from "farcaster-solidity/libraries/Ed25519.sol";
 
 library FarcasterSolidity {
-  error InvalidSignature();
-  error InvalidEncoding();
-  error InvalidMessageType();
+    error InvalidSignature();
+    error InvalidEncoding();
+    error InvalidMessageType();
 
-  function _verifyMessage(
-    bytes32 public_key,
-    bytes32 signature_r,
-    bytes32 signature_s,
-    bytes memory message
-  ) internal pure returns(MessageData memory) {
-    // Calculate Blake3 hash of FC message (first 20 bytes)
-    bytes memory message_hash = Blake3.hash(message, 20);
+    function _verifyMessage(bytes32 public_key, bytes32 signature_r, bytes32 signature_s, bytes memory message)
+        internal
+        pure
+        returns (bool)
+    {
+        // Calculate Blake3 hash of FC message (first 20 bytes)
+        bytes memory message_hash = Blake3.hash(message, 20);
 
-    // Verify signature
-    bool valid = Ed25519.verify(
-      public_key,
-      signature_r,
-      signature_s,
-      message_hash
-    );
-
-    if (!valid) {
-      revert InvalidSignature();
+        // Verify signature
+        return Ed25519.verify(public_key, signature_r, signature_s, message_hash);
     }
 
-    (
-      bool success,
-      ,
-      MessageData memory message_data
-    ) = MessageDataCodec.decode(0, message, uint64(message.length));
-
-    if (!success) {
-      revert InvalidEncoding();
+    function verifyFrameActionBodyMessage(
+        bytes32 public_key,
+        bytes32 signature_r,
+        bytes32 signature_s,
+        bytes memory message
+    ) external {
+        _verifyMessage(public_key, signature_r, signature_s, message);
     }
-
-    return message_data;
-  }
-
-  function _verifyMessage2(
-    bytes32 public_key,
-    bytes32 signature_r,
-    bytes32 signature_s,
-    bytes memory message
-  ) internal pure returns(MessageData memory) {
-    // Calculate Blake3 hash of FC message (first 20 bytes)
-    bytes memory message_hash = Blake3.hash(message, 20);
-
-    // Verify signature
-    bool valid = Ed25519.verify(
-      public_key,
-      signature_r,
-      signature_s,
-      message_hash
-    );
-
-    if (!valid) {
-      revert InvalidSignature();
-    }
-
-    (
-      bool success,
-      ,
-      MessageData memory message_data
-    ) = MessageDataCodec.decode(0, message, uint64(message.length));
-
-    if (!success) {
-      revert InvalidEncoding();
-    }
-
-    return message_data;
-  }
-
-  function verifyFrameActionBodyMessage(
-    bytes32 public_key,
-    bytes32 signature_r,
-    bytes32 signature_s,
-    bytes memory message
-  ) external {
-    MessageData memory message_data = _verifyMessage(
-      public_key,
-      signature_r,
-      signature_s,
-      message
-    );
-
-    if (message_data.type_ != MessageType.MESSAGE_TYPE_FRAME_ACTION) {
-      revert InvalidMessageType();
-    }
-  }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -3,12 +3,14 @@ pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {FarcasterSolidity} from "../src/FarcasterSolidity.sol";
+import "../src/Encoder.sol";
 
 contract CounterTest is Test {
-
     function test() public {
-        bytes memory message = hex"080d1095c90218e3d8a52e20028201330a1368747470733a2f2f6578616d706c652e636f6d10011a1a0895c902121473a773451c6b36d0a8dc8c806407a8a3f5f7da9e";
-        bytes memory sigHex = hex"68c44d89761d24d6a33f9fc0373a4fbc047c9f22835177d1a5eb33c91a0f2687be15a695fc7039845d9ce37f87d850cdef7f16b944a3d6f92a09825cb7a0fd09";
+        bytes memory message =
+            hex"080d1095c90218e3d8a52e20028201330a1368747470733a2f2f6578616d706c652e636f6d10011a1a0895c902121473a773451c6b36d0a8dc8c806407a8a3f5f7da9e";
+        bytes memory sigHex =
+            hex"68c44d89761d24d6a33f9fc0373a4fbc047c9f22835177d1a5eb33c91a0f2687be15a695fc7039845d9ce37f87d850cdef7f16b944a3d6f92a09825cb7a0fd09";
         (bytes32 r, bytes32 s) = abi.decode(sigHex, (bytes32, bytes32));
         bytes32 pk = 0x4fe8406370be9f8cbaddc6f0e909b2fc75c9d1f56d19395f9a90d9741b4f42d0;
         FarcasterSolidity.verifyFrameActionBodyMessage({
@@ -19,5 +21,36 @@ contract CounterTest is Test {
         });
     }
 
+    function test_2() public {
+        bytes memory b = MessageDataCodec.encode(
+            MessageData({
+                type_: MessageType.MESSAGE_TYPE_FRAME_ACTION,
+                fid: 64417,
+                timestamp: 97190733,
+                network: FarcasterNetwork.FARCASTER_NETWORK_TESTNET,
+                frame_action_body: FrameActionBody({
+                    // frame.base.org / chainId:calldata
+                    url: hex"68747470733a2f2f6578616d706c652e636f6d",
+                    button_index: 1,
+                    cast_id: CastId({fid: 64417, hash: hex"e9eca527e4d2043b1f77b5b4d847d4f71172116b"})
+                })
+            })
+        );
+        console2.logBytes(b);
+        assertEq(
+            b,
+            hex"080d10a1f70318cd86ac2e20028201330a1368747470733a2f2f6578616d706c652e636f6d10011a1a08a1f7031214e9eca527e4d2043b1f77b5b4d847d4f71172116b"
+        );
 
+        bytes memory sigHex =
+            hex"17bdafddf9cf7464959a28d57fb5da7c596de4796f663588ea24d804c13ca043f46a546ca474d1b4420cc48e8720d8051786b21a689cdf485f78e51e36a12b05";
+        (bytes32 r, bytes32 s) = abi.decode(sigHex, (bytes32, bytes32));
+        bytes32 pk = 0x292404752ddd67080bbfe93af4017e51388ebc3c9fb96b8984658155de590b38;
+        FarcasterSolidity.verifyFrameActionBodyMessage({
+            public_key: pk,
+            signature_r: r,
+            signature_s: s,
+            message: b
+        });
+    }
 }
