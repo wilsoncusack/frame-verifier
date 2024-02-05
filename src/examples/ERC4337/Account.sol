5df97ce13264db636d53c5f5acf5a823ab2034ab
Wilson Cusack <cusackw@gmail.com>
2024-02-05 13:05:42 -0500

    quick example of using in 4337 context

--- src/examples/ERC4337/Account.sol
+++ src/examples/ERC4337/Account.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.9;
 
 import {MessageData} from "../../FrameVerifier.sol";
-// import Strings
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 import {UserOperation} from "./UserOperation.sol";
 import {SharedVerifier} from "../SharedVerifier.sol";
@@ -23,21 +23,24 @@ contract Account {
         external
         returns (uint256 validationData)
     {
+        // check msg.sender is entrypoint
+
         FrameUserOpSignature memory frameStruct = abi.decode(userOp.signature, (FrameUserOpSignature));
-        string memory expectedUrl = string.concat(baseUrl, Strings.toString(block.chainId), ":", userOp.callData);
-        frameStruct.messageData.frame_action_body.url = expectedUrl;
+        string memory expectedUrl =
+            string.concat(baseUrl, Strings.toString(block.chainid), ":", string(userOp.callData));
+        frameStruct.messageData.frame_action_body.url = bytes(expectedUrl);
 
         if (
-            SharedVerifier.verifyMessageData(
+            sharedVerifier.verifyMessageData(
                 SharedVerifier.VerifyRequest(
                     publicKey, frameStruct.signature_r, frameStruct.signature_s, frameStruct.messageData
                 )
             )
         ) {
-            if (frameStruct.messageData.frame_action_body.timestamp < lastFrameTimstamp) {
+            if (frameStruct.messageData.timestamp < lastFrameTimstamp) {
                 return 1;
             }
-            lastFrameTimstamp = frameStruct.messageData.frame_action_body.timestamp;
+            lastFrameTimstamp = frameStruct.messageData.timestamp;
             return 0;
         }
 
--- src/examples/ERC4337/Paymaster.sol
+++ src/examples/ERC4337/Paymaster.sol
@@ -2,23 +2,75 @@
 pragma solidity ^0.8.9;
 
 import {UserOperation} from "./UserOperation.sol";
+import {Account} from "./Account.sol";
 import {SharedVerifier} from "../SharedVerifier.sol";
 
 contract Paymaster {
     mapping(bytes32 => uint256) public castBalance;
     mapping(uint256 => uint256) public fidBalance;
 
-    function validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 maxCost)
+    enum SponsorType {
+        cast,
+        fid
+    }
+
+    SharedVerifier public sharedVerifier;
+
+    function validatePaymasterUserOp(UserOperation calldata userOp, bytes32, uint256 maxCost)
         external
         returns (bytes memory context, uint256 validationData)
-    {}
+    {
+        // check msg.sender is entrypoint
 
-    function postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) external {}
+        Account.FrameUserOpSignature memory frameStruct = abi.decode(userOp.signature, (Account.FrameUserOpSignature));
+
+        // assume invalid
+        validationData = 1;
+
+        if (
+            sharedVerifier.verifyMessageData(
+                SharedVerifier.VerifyRequest(
+                    Account(userOp.sender).publicKey(),
+                    frameStruct.signature_r,
+                    frameStruct.signature_s,
+                    frameStruct.messageData
+                )
+            )
+        ) {
+            bytes32 castHash = bytes32(frameStruct.messageData.frame_action_body.cast_id.hash);
+            if (castBalance[castHash] >= maxCost) {
+                castBalance[castHash] -= maxCost;
+                validationData = 0;
+                context = abi.encode(maxCost, uint256(castHash), SponsorType.cast);
+            }
+
+            if (fidBalance[frameStruct.messageData.fid] >= maxCost) {
+                fidBalance[frameStruct.messageData.fid] -= maxCost;
+                validationData = 0;
+                context = abi.encode(maxCost, frameStruct.messageData.fid, SponsorType.fid);
+            }
+        }
+    }
+
+    function postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) external {
+        // check msg.sender is entrypoint
+
+        if (mode == PostOpMode.postOpReverted) {
+            return;
+        }
+
+        (uint256 maxCost, uint256 identifier, SponsorType sType) = abi.decode(context, (uint256, uint256, SponsorType));
+        uint256 credit = maxCost - actualGasCost;
+        if (sType == SponsorType.cast) {
+            castBalance[bytes32(identifier)] += credit;
+        } else {
+            fidBalance[identifier] += credit;
+        }
+    }
 
     enum PostOpMode {
-        opSucceeded, // user op succeeded
-        opReverted, // user op reverted. still has to pay for gas.
-        postOpReverted // user op succeeded, but caused postOp to revert
-
+        opSucceeded,
+        opReverted,
+        postOpReverted
     }
 }
